fc3385bf22f3e9f3d6f3576e5c8ae7eaa63174f5
JakubKucharski9 <s27445@pjwstk.edu.pl>
2023-03-19 13:13:23 +0100

    zadanie1/4

---
+++ Zajecia4/z1php.php
@@ -0,0 +1,22 @@
+<?php
+$liczba1=$_GET['liczba1'];
+$liczba2=$_GET['liczba2'];
+$dzialanie=$_GET['dzialanie'];
+$wynik=0;
+switch ($dzialanie){
+    case "Dodawanie":
+        $wynik=$liczba1+$liczba2;break;
+    case 'Odejmowanie':
+        $wynik=$liczba1-$liczba2;break;
+    case 'Mnozenie':
+        $wynik=$liczba1*$liczba2;break;
+    case 'Dzielenie':
+        if($liczba2==0){
+            echo "Nie dziel przez 0";
+            exit;
+        } else {
+            $wynik=$liczba1/$liczba2;break;
+        }
+}
+print 'Wynik dzialania: '.$wynik;
+
